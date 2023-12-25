e700c9de4280216105ceccebd0f6d1303903c95c
Bharanitharan.Ragu <bharani3ran@gmail.com>
2023-12-25 15:55:10 -0500

    initial

---
+++ src/test/java/com/batty/framework/service/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batty.framework.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
